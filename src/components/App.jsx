d273477e0a1ec4458f0416f3827d8891088aa0e2
Hack Reactor Student <student@hackreactor.com>
2017-10-25 19:58:57 -0700

    Switching Videos working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -21,8 +21,16 @@ class App extends React.Component {
     super(props);
     
     this.state = {
-      active: true
+      video: exampleVideoData[0]
+      //id
+      //descript exampledata[].descript
     };
+  }
+  
+  onVideoClick (event) {
+    this.setState({
+      video: event
+    });
   }
   
   render() {
@@ -31,15 +39,15 @@ class App extends React.Component {
       <div>
         <nav className="navbar">
           <div className="col-md-6 offset-md-3">
-            <div><h5><em>search</em> view goes here</h5></div>
+            <div><h5> <Search/></h5></div>
           </div>
         </nav>
         <div className="row">
           <div className="col-md-7">
-            <div><h5><em>videoPlayer</em> <VideoPlayer video={exampleVideoData[0]}/> </h5></div>
+            <div><h5><VideoPlayer video={this.state.video}/> </h5></div>
           </div>
           <div className="col-md-5">
-            <div><h5><em>videoList</em> <VideoList videos={exampleVideoData}/> </h5></div>
+            <div><h5> <VideoList onClickFunction={this.onVideoClick.bind(this)} videos={exampleVideoData}/> </h5></div>
           </div>
         </div>
       </div>
@@ -49,6 +57,8 @@ class App extends React.Component {
   }
   
 }
+
+
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
